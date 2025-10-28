c1badc6c25fba06ef24ef2629c448f722907415d
Sidharth Shamshabad <sid@techvoe.com>
2025-10-27 20:16:12 -0400

    Create navbar component and app frontend structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
 
 export default function Home() {
     const [message, setMessage] = useState('Loading...');
@@ -17,7 +18,7 @@ export default function Home() {
 
     return (
         <main className=''>
-            <div>navbar</div>
+            <Navbar />
             <div>sidebar</div>
             <div>
                 <p>{message}</p>
